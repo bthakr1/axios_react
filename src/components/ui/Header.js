706e01f14794910bca0709620a5929e1a0a048fa
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-08-22 14:01:36 -0500

    added css and app.js with Header.js

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../image/logo3.png';
+
+const header = () => {
+    return (
+        <header className='center'>
+            <img src={logo} alt='' />
+            
+        </header >
+    )
+}
+
+export default header
